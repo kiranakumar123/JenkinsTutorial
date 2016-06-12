08927abd61df80207117dc976270d232a2a1b43d
kiran_akumar123 <kiran.akumar123@gmail.com>
2016-06-12 07:35:59 +0000

    Again

--- AUPSchedulerAutomationFramework/src/com/kriyari/selenium/testing/SeGridTest.java
+++ AUPSchedulerAutomationFramework/src/com/kriyari/selenium/testing/SeGridTest.java
@@ -35,8 +35,8 @@ public class SeGridTest
          //       driver=TheSeleniumManager.getSeleniumManager().getDriver("firefox");
 String baseSeleniumDir = System.getenv("SELENIUM_HOME");
                 System.out.println("inside se grid selenium_home:"+baseSeleniumDir);
-String baseSeleniumDir = System.getenv("SELENIUM_REPORTS_HOME");
-                System.out.println("inside se grid selenium_reports_home:"+baseSeleniumDir);
+String newbaseSeleniumDir = System.getenv("SELENIUM_REPORTS_HOME");
+                System.out.println("inside se grid selenium_reports_home:"+newbaseSeleniumDir);
                 logger=TheSeleniumManager.getSeleniumLogger("testSeleniumLogger");
 
         }
